740a7a2b70d6dad7f755a27aa82e6f344dc991f2
Srikanth R <srikanthalways@gmail.com>
2017-11-18 19:44:01 +0530

    Component 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
 
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import {MatListModule} from '@angular/material';
+
+import {MatLineModule} from '@angular/material';
+
+import 'hammerjs';
+
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
+    ,BrowserAnimationsModule
+    ,FlexLayoutModule
+    ,MatToolbarModule
+    ,MatListModule
+    ,MatLineModule
   ],
   providers: [],
   bootstrap: [AppComponent]
